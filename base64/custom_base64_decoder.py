6460f1ecdf53844e19b6d90ea1ded39e4d6e2027
Rodrigo Ortega <91147909+RodrigoOrtega1@users.noreply.github.com>
2025-08-31 19:34:31 -0600

    fix: decodifica un archivo dado

--- base64/custom_base64_decoder.py
+++ base64/custom_base64_decoder.py
@@ -1,12 +1,13 @@
 def custom_base64_decode(input_string):
-    """
-    Decodes a Base64 encoded string step by step as described in the example.
+    """ Decodifica una cadena codificada en base 64 como lo describe el siguiente articulo
+
+    https://base64.guru/learn/base64-algorithm/decode
 
     Args:
-        input_string (str): The Base64 encoded string to decode.
+        input_string (str): Una cadena codificada en base 64.
 
     Returns:
-        str: The decoded ASCII string.
+        str: La cadena decodificada.
     """
     base64_to_value = {
         "A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7,
@@ -32,3 +33,30 @@ def custom_base64_decode(input_string):
     decoded_string = "".join(decoded_characters)
 
     return decoded_string
+
+def read_file_by_byte(filename):
+    """ Lee un archivo por bytes
+
+    Args:
+        filename (str): El nombre del archivo
+    """
+    with open(filename, 'rb') as f:
+        return f.read().decode('ascii')
+    
+def save_decoded_bytes(decoded_bytes, destination_filename):
+    """ Guarda una cadena de bytes en un archivo
+
+    Args:
+        decoded_bytes (str): Cadena a guardar
+        destination_filename(str): Nombre del archivo donde se guardara
+    """
+    with open(destination_filename, 'wb') as f:
+        f.write(decoded_bytes.encode('latin1'))
+
+def main():
+    bytes = read_file_by_byte('file3.lol')
+    decoded = custom_base64_decode(bytes)
+    save_decoded_bytes(decoded, 'decoded_file3.mp4')
+
+if __name__=="__main__":
+    main()
